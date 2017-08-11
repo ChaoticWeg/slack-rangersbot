b97dede8856e5f0f9519a4ce39918336148d72b9
Shawn Lutch <shawn@chaoticweg.cc>
2017-08-11 17:41:17 -0500

    handle Watcher 'play' event

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Watcher   = require('./lib/watcher/Watcher.js');
 const Logger    = require('./lib/logging/Logger.js');
+const Timestamp = require('./lib/Timestamp.js');
 const Constants = require('./lib/Constants.js');
 
 var logger  = new Logger("MAIN");
@@ -7,6 +8,10 @@ var watcher = new Watcher();
 
 watcher.on('data', data => {
     logger.debug('Data event intercepted');
+});
+
+watcher.on('play', play => {
+    logger.info(play.result.description.trim());
 });
 
 watcher.gameday.getGameByTeamId(Constants.TeamID).then(
