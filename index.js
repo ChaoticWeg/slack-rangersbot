b36db971de18d0470fb6525df0675d749cefaf7f
Shawn Lutch <shawn@chaoticweg.cc>
2017-08-11 15:09:20 -0500

    Add executing file

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const Watcher = require('./lib/watcher/Watcher.js');
+const Constants = require('./lib/Constants.js');
+const Watcher   = require('./lib/watcher/Watcher.js');
 
+// TODO get rangers game ID from scoreboard
 var watcher = new Watcher();
+
+watcher.gameday.getGameByTeamId(Constants.TeamID).then(
+
+    (data) => {
+        if (!data.gamePk)
+        {
+            console.error("The requested team does not play today.");
+            return;
+        }
+
+        watcher.start(data.gamePk);
+    },
+
+    (err) => {
+        console.error(err);
+    }
+
+);
