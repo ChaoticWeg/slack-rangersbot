9cd4b9cfa877bccfbfb2be558363913242eee13d
Shawn Lutch <shawn@chaoticweg.cc>
2017-08-11 18:54:13 -0500

    Adjust event handlers

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+const Slack     = require('./lib/slack/SlackHandler.js');
 const Watcher   = require('./lib/watcher/Watcher.js');
 const Logger    = require('./lib/logging/Logger.js');
 const Timestamp = require('./lib/Timestamp.js');
 const Constants = require('./lib/Constants.js');
+const fs        = require('fs');
 
 var logger  = new Logger("MAIN");
 var watcher = new Watcher();
+var slack   = new Slack();
 
-watcher.on('data', data => {
-    logger.debug('Data event intercepted');
-});
+watcher.on('data', data => fs.writeFileSync('./tmp/last-data.json', JSON.stringify(data, null, 4)));
 
 watcher.on('play', play => {
-    logger.info(play.result.description.trim());
+    logger.info(`New play! ${play.result.description.trim()}`);
+    slack.announce(play.result.description.trim());
 });
 
 watcher.gameday.getGameByTeamId(Constants.TeamID).then(
